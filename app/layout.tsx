9ea7884de7a356877322924423b0546fd7fcab4b
Nayan Das <nayan010das@gmail.com>
2024-12-19 18:02:16 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +18,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <ClerkLoading>
+          <div className="flex items-center justify-center h-screen text-2xl">
+            LOADING...
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <div className="max-w-6xl mx-auto">
+            <div className="flex flex-col h-screen">
+              <Navbar />
+              {children}
+            </div>
+          </div>
+        </ClerkLoaded>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
